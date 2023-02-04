684708d6a08b8ad58475cf1a1d18a343db57ce2e
leeaoyin <lee_ay@163.com>
2023-02-04 16:57:39 +0800

    feat: add antd

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { message, Popconfirm, Button } from 'antd';
 
 export default class Item extends Component {
 
@@ -16,22 +17,39 @@ export default class Item extends Component {
     }
   }
   handleDelete = (id) => {
-    const isDel = window.confirm('确定删除吗?');
-    if (isDel) {
-      this.props.deleteTodo(id);
-    }
+    this.props.deleteTodo(id);
 
+  }
+  confirm = (e) => {
+    const { id } = this.props;
+    this.props.deleteTodo(id);
+    message.success('已删除');
+  }
+  cancel = () => {
+    message.error('已取消');
   }
   render() {
     const { id, name, done } = this.props;
     const { mouse } = this.state;
     return (
-      <li style={{ backgroundColor: mouse ? '#4e90f5' : 'white', color: mouse ? 'white' : 'black' }} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)} >
+      <li style={{ backgroundColor: mouse ? '#a1caf6' : 'white', color: mouse ? 'white' : 'black' }} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)} >
         <label>
           <input type="checkbox" checked={done ? true : false} onChange={this.handleChecked(id)} />
           <span>{name}</span>
         </label>
-        <button onClick={() => { this.handleDelete(id) }} className="btn btn-danger" style={{ display: mouse ? 'block' : 'none' }}>删除</button>
+        {/* <button onClick={() => { this.handleDelete(id) }} className="btn btn-danger" style={{ display: mouse ? 'block' : 'none' }}>删除</button> */}
+        <div style={{ display: mouse ? 'block' : 'none', textAlign: 'right' }}>
+          <Popconfirm
+            title="删除"
+            description="你确定要删除这一项吗?"
+            onConfirm={this.confirm}
+            // onCancel={this.cancel}
+            okText="是"
+            cancelText="否"
+          >
+            <Button type="link" style={{ color: mouse ? 'red' : 'black' }}>删除</Button>
+          </Popconfirm>
+        </div>
       </li>
     )
   }
