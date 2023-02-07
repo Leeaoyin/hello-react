efb08124bdb471057b54da037121292eed1fe70f
leeaoyin <lee_ay@163.com>
2023-02-07 14:48:20 +0800

    simplify navlink

--- src/components/pages/MyNavLink/index.jsx
+++ src/components/pages/MyNavLink/index.jsx
@@ -6,7 +6,8 @@ export default class MyNavLink extends Component {
     const{children} = this.props;
     return (
       <div>
-        <NavLink className="list-group-item" {...this.props}>{children}</NavLink>
+        {/* <NavLink className="list-group-item" {...this.props}>{children}</NavLink> */}
+        <NavLink className="list-group-item" {...this.props} />
       </div>
     )
   }
